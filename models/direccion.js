c46f697ca72868a791952f340d32453da397819c
sorvylenny <115513656+sorvylenny@users.noreply.github.com>
2024-03-08 22:31:44 -0500

    Revert "Revert "Maquetacion create, edit inmueble""
    This reverts commit 5ef9265ab1251ec60b740af64cb3e67d35d97338.

--- models/direccion.js
+++ models/direccion.js
@@ -9,7 +9,7 @@ const addressSchema = new mongoose.Schema({
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
-  createdBy: { type: mongoose.Schema.Types.Mixed, required: true },
+  createdBy: { type: mongoose.Schema.Types.Mixed, required: false },
   updatedAt: { type: Date, default: Date.now },
   updatedBy: { type: mongoose.Schema.Types.Mixed, required: false },
   bedrooms: { type: Number, required: true },
